e3fa2b7114e76c448eaec322e2189a7fe519f331
David Reepmeyer <djreep81@gmail.com>
2014-06-09 00:24:28 -0500

    added chagnes for X11

--- CMake/Packages/FindOIS.cmake
+++ CMake/Packages/FindOIS.cmake
@@ -6,29 +6,25 @@ FIND_LIBRARY(OIS_LIBRARY NAMES OIS)
 
 
 IF (OIS_INCLUDE_DIR AND OIS_LIBRARY)
-   SET(OIS_FOUND TRUE)
- ENDIF (OIS_INCLUDE_DIR AND OIS_LIBRARY)
+  SET(OIS_FOUND TRUE)
+ENDIF (OIS_INCLUDE_DIR AND OIS_LIBRARY)
 
 IF (OIS_FOUND)
-
-#if (cpp-commons_FOUND)
-#  include_directories(${cpp-commons_INCLUDE_DIRS})
-#  target_link_libraries (steering-behaviors ${cpp-commons_LIBRARIES})
-#endif (cpp-commons_FOUND)
-
-   # show which OIS was found only if not quiet
-   IF (NOT OIS_FIND_QUIETLY)
-      MESSAGE(STATUS "Found OIS: ${OIS_LIBRARY}")
-      MESSAGE(STATUS "Found OIS: ${OIS_INCLUDE_DIR}")
-      #include_directories(${OIS_INCLUDE_DIR})
-      #target_link_libraries(steering-behaviors ${OIS_INCLUDE_DIR})
-   ENDIF (NOT OIS_FIND_QUIETLY)
+  set(OIS_LIBRARIES ${OIS_LIBRARY})
+  if (UNIX)
+    set(OIS_LIBRARIES ${OIS_LIBRARIES} X11)
+  endif (UNIX)
+  IF (NOT OIS_FIND_QUIETLY)
+    MESSAGE(STATUS "Found OIS: ${OIS_LIBRARY}")
+    MESSAGE(STATUS "Found OIS: ${OIS_LIBRARIES}")
+    MESSAGE(STATUS "Found OIS: ${OIS_INCLUDE_DIR}")
+  ENDIF (NOT OIS_FIND_QUIETLY)
 
 ELSE (OIS_FOUND)
 
-   # fatal error if OIS is required but not found
-   IF (OIS_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find OIS")
-   ENDIF (OIS_FIND_REQUIRED)
+  # fatal error if OIS is required but not found
+  IF (OIS_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find OIS")
+  ENDIF (OIS_FIND_REQUIRED)
 
 ENDIF (OIS_FOUND)
